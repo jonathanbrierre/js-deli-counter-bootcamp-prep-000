cae639d2e82e794eb1bc29e4d161a338e19b19a5
jonathan brierre <jonathanbrierre@yahoo.com>
2019-11-05 01:15:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ function currentLine(line){
   if (0 < line.length){
    let i = 0;
    while (i < line.length){
-     string = string + ` ${i+ 1}. ${line[i]}`;
+     string = string + `${i+ 1}. ${line[i]}`;
      i++;
      
    }
